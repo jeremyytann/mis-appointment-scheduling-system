68e5249cca5b2791e79fede7537f0a0f997bb31f
Jeremy <jeremyyytannn.work@gmail.com>
2026-06-12 19:08:13 +0800

    chore: setup appointments module dependencies and wiring

--- src/modules/appointments/appointments.module.ts
+++ src/modules/appointments/appointments.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Appointment } from './entities/appointment.entity';
+import { AppointmentsService } from './appointments.service';
+import { AppointmentsController } from './appointments.controller';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Appointment])],
+  providers: [AppointmentsService],
+  controllers: [AppointmentsController],
 })
 export class AppointmentsModule {}
